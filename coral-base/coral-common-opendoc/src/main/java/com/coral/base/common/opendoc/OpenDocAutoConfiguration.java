8ddbc599d6edd3412d9e11af0f8febdd90f832b7
huss <coralloc8@dingtalkcom>
2023-08-29 08:42:20 +0800

    opendoc提取成module

---
+++ coral-base/coral-common-opendoc/src/main/java/com/coral/base/common/opendoc/OpenDocAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.coral.base.common.opendoc;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author huss
+ * @version 1.0
+ * @className OpenDocAuto
+ * @description todo
+ * @date 2021/9/18 10:04
+ */
+@ComponentScan
+public class OpenDocAutoConfiguration {
+}
